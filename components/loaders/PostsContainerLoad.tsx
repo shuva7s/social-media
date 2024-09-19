a0f6e19f2c735b401c7d07dc938fb62a8c28f945
Shuvadeep Mandal <shuvadeepmandal5@gamil.com>
2024-09-19 11:56:43 +0530

    porojects loader's columns updated

--- components/loaders/PostsContainerLoad.tsx
+++ components/loaders/PostsContainerLoad.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "../ui/skeleton";
 
 const PostsContainerLoad = () => {
   return (
-    <div className="columns-1 md:columns-2 lg:columns-3 space-y-2 gap-2 mt-4">
+    <div className="columns-1 md:columns-2 xl:columns-3 space-y-2 gap-2 mt-4">
       <Skeleton className="min-h-96 break-inside-avoid" />
       <Skeleton className="min-h-32 break-inside-avoid" />
       <Skeleton className="min-h-64 break-inside-avoid" />
